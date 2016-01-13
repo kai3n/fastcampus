328286efbc2eaceda27faf3109c556b06b6270b5
deadlylaid <deadlylaid@gmail.com>
2016-01-13 15:19:01 +0900

    minsoo

--- minsooHan.py
+++ minsooHan.py
@@ -8,36 +8,13 @@ print (word_count('I am a boy'))
 '''
 def search(string, word):
 
-    if type(string) == list:
+
+    if type(string) == list or type(string) == tuple or type(string) ==set or type(string)==str:
         mon = list(string)
         if word in mon:
             return True
         else:
             return False
-
-
-    elif type(string) == tuple:
-        mon = list(string)
-        if word in mon:
-            return True
-        else :
-            return False
-
-    elif type(string) == set:
-        mon = list(string)
-        if word in mon:
-            return True
-        else :
-            return False
-
-    elif type(string) == str:
-        mon = set(string)
-        mon_word = set(word)
-        if mon_word == mon:
-            return True
-        else :
-            return False
-
 
     else :
         return False
@@ -45,8 +22,7 @@ def search(string, word):
 
 
 
-print(search([1,2],1))
 print(search({1,2},1))
 print(search((1,2),1))
-print(search('aaa','aa'))
-print(search('aaaa','aa'))
+print(search('aaa','a'))
+print(search('afefa','a'))

1c380e28135ad381a2e5c79792ec077402c0a4bf
deadlylaid <deadlylaid@gmail.com>
2016-01-13 14:40:44 +0900

    minsoo

--- minsooHan.py
+++ minsooHan.py
@@ -1,24 +1,47 @@
+'''
 def word_count(word):
     word_cnt=word.split(" ")
 
     return len(word_cnt)
 
 print (word_count('I am a boy'))
-
+'''
 def search(string, word):
-    match = 0
     list_check=[1,2]
     tuple_check=(1,2)
     set_check={1,2}
+    string_check='check'
 
     if type(string) == type(list_check):
-        return True
+        mon = list(string)
+        if word in mon:
+            return True
+        else:
+            return False
+
 
     elif type(string) == type(tuple_check):
-        return True
+        mon = list(string)
+        if word in mon:
+            return True
+        else :
+            return False
 
     elif type(string) == type(set_check):
-        return True
+        mon = list(string)
+        if word in mon:
+            return True
+        else :
+            return False
+
+    elif type(string) == type(string_check):
+        mon = set(string)
+        mon_word = set(word)
+        if mon_word == mon:
+            return True
+        else :
+            return False
+
 
     else :
         return False
@@ -26,4 +49,5 @@ def search(string, word):
 
 
 
-print(search([1,2],'s'))
+print(search((1,2),1))
+print(search('aaa','aa'))

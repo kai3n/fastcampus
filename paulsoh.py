b4ad077a60e572a4ecd4cbccf1a8a31c746f7065
yongdoree <yongdoree@gmail.com>
2016-01-13 14:37:51 +0900

    Merge branch 'master' of https://github.com/kai3n/fastcampus
    # Conflicts:
    #	std1.py

--- paulsoh.py
+++ paulsoh.py
@@ -10,10 +10,26 @@ def word_count(word):
     return len(word.split())
 
 
-def range(a,b):
+def range_function(a, *args):
+    result = []
+    if len(args) == False:
+        print(range(0,a))
+        return range(0,a)
+    elif len(args) == 1:
+        print(range(a, args[0]))
+        return range(a,args[0])
+    elif len(args) == 2:
+        print(range(a, args[0], args[1]))
+        return range(a, args[0], args[1])
 
 
 
+range_function(5)
+range_function(0,7)
+range_function(0,10,2)
+
+
+'''
 test_string = 'what is my name my name is paul soh hello world'
 query_for_test_string = 'name'
 
@@ -34,3 +50,4 @@ print(search(test_dict, query_for_test_dict))
 #print(dosearch(string, 'is'))
 #print(dosearch(string, 'iadfasdfas'))
 
+'''

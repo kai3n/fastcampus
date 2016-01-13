5c1e6a5f65ef9102f347349c984feea5d8d10df6
yongdoree <yongdoree@gmail.com>
2016-01-13 14:07:10 +0900

    Merge branch 'master' of https://github.com/kai3n/fastcampus
    # Conflicts:
    #	std1.py

--- paulsoh.py
+++ paulsoh.py
@@ -1,9 +1,35 @@
 
 
+
+def search(list, word):
+    try:
+       return bool(list.count(word))
+    except:
+        print("Input not valid, try again")
+        return False
+        
 def word_count(word):
     return len(word.split())
 
 
-string = 'what is my name my name is paul soh hello world'
 
-print(word_count(string))
+test_string = 'what is my name my name is paul soh hello world'
+query_for_test_string = 'name'
+
+test_list = ['a', 'ab', 'ac', 'add']
+query_for_test_list = 'ac'
+
+test_dict = {'a':'b', 'c':'d'}
+query_for_test_dict = 'a'
+
+#print(word_count(test_string))
+
+
+print(search(test_string, query_for_test_string))
+print(search(test_list, query_for_test_list))
+print(search(test_dict, query_for_test_dict))
+
+
+#print(dosearch(string, 'is'))
+#print(dosearch(string, 'iadfasdfas'))
+

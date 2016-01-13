5afd0d2abc6ff38d001159ae2befec62343e79a2
goohooh <goo_oo@naver.com>
2016-01-13 13:36:44 +0900

    test

--- SeongPil_Lee.py
+++ SeongPil_Lee.py
@@ -4,4 +4,4 @@ def word_count(word):
     word_cnt = len(words.split(' '))
     return word_cnt
 
-print(word_count('어려서부터 우리집은 가난했었고, 남들 가는 외식 한번 간적 없었고~'))
+print(word_count('어려서부터 우리집은 가난했었고, 남들 가는 외식 한번 간적 없었고'))

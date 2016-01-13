3e0cd96d5d026dc111ce146459c0ec62f81db087
JEON <MJ@JEONui-MacBook-Pro.local>
2016-01-13 13:51:13 +0900

    1. Count number of words in a sentence 2. Search function

--- mhjeon.py
+++ mhjeon.py
@@ -4,3 +4,11 @@ def word_count(sentence):
     return len(sentence.split())
 
 print(word_count(sentence))
+
+def search(string, word):
+    if type(string) == list or str or tuple:
+        #string인자를 list로 타입 캐스트한다. 리스트, 문자열, 튜플은 모두 리스트 가능하다.
+        if word in list(string):
+            return True
+    else:
+        return False

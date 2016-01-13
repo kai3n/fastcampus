8dc03797cb649be0b9d6f233db553d5c76746eb7
sodarain730 <sodarain730@gmail.com>
2016-01-13 14:51:17 +0900

    test

--- leejeonghyeon.py
+++ leejeonghyeon.py
@@ -3,6 +3,12 @@ def word_count(word):
     return count_cnt
 print(word_count("a b c d"))
 
-def word_sr(w01, w02):
+def word_search(w01, w02):
 
-word_sr()
+    for ww01,ww02 in w01.items():
+        if w02 in ww01:
+            return True
+        return False
+
+print(word_search({'a':'b','c':'d'},'c'))
+print(word_search({'a':'b','c':'d'},'u'))

8b6e01aa1e1e7f0c5b34bfb99be1027dccb5ab32
goohooh <goo_oo@naver.com>
2016-01-13 21:45:03 +0900

    function

--- SeongPil_Lee.py
+++ SeongPil_Lee.py
@@ -6,52 +6,68 @@ def word_count(word):
 
 print(word_count('어려서부터 우리집은 가난했었고, 남들 가는 외식 한번 간적 없었고'))
 
-def search(string, word):
-    if type(string) == dict:
-        return False
-    elif type(string) == str:
-        if word in string:
-            return True
-        else:
-            return False
-    elif type(string) == list:
-        for item in string:
-            if word in item:
-                return True
-        if word in string:
-            return True
-        else:
-            return False
-    else:
-        return False
+# def search(string, word):
+    # if type(string) == dict:
+    #     return False
+    # elif type(string) == str:
+    #     if word in string:
+    #         return True
+    #     else:
+    #         return False
+    # elif type(string) == list:
+    #     if type(word) == str:
+    #         for item in string:
+    #             if word in item:
+    #                 return True
+    #     else:
+    #         if word in string:
+    #             return True
+    #         else:
+    #             return False
+    #     if word in string:
+    #         return True
+    #     else:
+    #         return False
+    # else:
+    #     return False
 
-print(search(['apple', 1], 'ap'))
+    # if type(string) == str:
+    #     if word in string:
+    #         return True
+    #     else:
+    #         return False
+    # elif type(string) == tuple or type(string) == list:
+
+# print(search(['apple', 1, 0.1], '1'))
 
 def num_range(start, end, step):
     num_list = []
-    if start > end:
-        max = start
-        min = end
-    elif start < end:
-        max = end
-        min = start
-    elif start == end:
-        return []
+    if step == 0:
+        return print('error: step must not be Zero')
+
+    if start < end:
+        if step > 0:
+            while start < end:
+                num_list.append(start)
+                start += step
+            return num_list
+        elif step < 0:
+            return []
+        else:
+            print('error: I don\'t know what happened')
+    elif start > end:
+        if step < 0:
+            while start > end:
+                num_list.append(start)
+                start += step
+            return num_list
+        elif step > 0:
+            return []
+        else:
+            print('error: I don\'t know what happened')
     else:
         return []
 
-    if step > 0:
-        while min < max:
-            num_list.append(min)
-            min += step
-            return num_list
-    elif step < 0:
-        while max > min:
-            num_list.append(max)
-            max += step
-            return num_list
-    elif step == 0:
-        return print('error')
+print(num_range(10,10,0))
 
 
-print(num_range(0,10,-2))

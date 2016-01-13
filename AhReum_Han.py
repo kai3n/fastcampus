7f65b3fed9cf1a6a4973a20ff6b8a42e42166060
autumnhan <genius2614@gmail.com>
2016-01-13 14:33:10 +0900

    test

--- AhReum_Han.py
+++ AhReum_Han.py
@@ -1,7 +1,15 @@
 #글자 수 세기
 #특정 문자열을 매개변수로 넣기 매개변수로 넣으면 길이를 반환
 
-a = 'python is too hard'
-print(a.count(''))
+#a = 'python is too hard'
+#print(a.count(''))
+
+def word_count(word):
+ word_cnt=word.split(" ")
+
+ return len(word_cnt)
+
+print(word_count('python is too hard'))
+
 
 

ed39c0562e8ed0d9824c2c4344d9699034ea4aa0
Mingun Pak <kaienmg@gmail.com>
2016-01-13 22:48:53 +0900

    1/13일 과제입니다.

--- kaien.py
+++ kaien.py
@@ -1,4 +1,24 @@
-def word_count(word):
-    word_cnt = 0
-    return word_cnt
-## haha
+# 다음 함수를 구현하시오
+def wd_upper(word):
+    """
+    이 함수는 word 문자열(str type) 전체를 대문자로 변환하는 함수이다.
+    매개변수가 str 타입이 아닐 시 None을 반환한다.
+    ex1) wd_upper('apple')를 호출할 시 'APPLE'를 반환한다.
+    ex2) wd_upper('Plane')를 호출할 시 'PLANE'를 반환한다.
+    ex3) wd_upper('AbCdE')를 호출할 시 'ABCDE'를 반환한다.
+    :param word:
+    :return word:
+    """
+    pass
+def wd_lower(word):
+    """
+    이 함수는 word 문자열(str type) 전체를 소문자로 변환하는 함수이다.
+    매개변수가 str 타입이 아닐 시 None을 반환한다.
+    ex1) wd_upper('apple')를 호출할 시 'apple'를 반환한다.
+    ex2) wd_upper('Plane')를 호출할 시 'plane'를 반환한다.
+    ex3) wd_upper('AbCdE')를 호출할 시 'abcde'를 반환한다.
+
+    :param word:
+    :return word:
+    """
+    pass

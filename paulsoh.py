d26251eac074e30663d9b3d9b33d49e310749875
yongdoree <yongdoree@gmail.com>
2016-01-13 15:42:58 +0900

    Merge branch 'master' of https://github.com/kai3n/fastcampus
    # Conflicts:
    #	std1.py

--- paulsoh.py
+++ paulsoh.py
@@ -1,40 +1,93 @@
-def search(list, word):
-    try:
-        return bool(list.count(word))
-    except:
-        print("Input not valid, try again")
+# def search(list, word):
+#     if type(input) == str:
+#         if word in list:
+#             return True
+#     elif type(list) == tuple or type(list) == list:
+#         buff = ""
+#         for i in list:
+#             buff += str(i)
+#         if word in buff:
+#             return True
+#     return False
+#
+#
+# def word_count(word):
+#     return len(word.split())
+#
+
+def range_function(a, *args):
+    a = int(a)
+    result = []
+    counter = 0
+    if len(args) == False:
+        if a > 0:
+            while(counter < a):
+                result.append(counter)
+                counter += 1
+            return result
+        elif a < 0:
+            while(counter > a):
+                result.append(counter)
+                counter -= 1
+            return result
+        else:
+            return False
+
+    elif len(args) == 1:
+        if a < args[0]:
+            counter = a
+            while(counter < args[0]):
+                result.append(counter)
+                counter += 1
+            return result
+        elif a > args[0]:
+            counter = a
+            while(counter > args[0]):
+                result.append(counter)
+                counter -= 1
+            return result
+        else:
+            return False
+
+    elif len(args) == 2:
+        counter = a
+        if a < args[0]:
+            while(counter < args[0]):
+                if args[1] < 0:
+                    return False
+                result.append(counter)
+                counter += args[1]
+            return result
+        elif a > args[0]:
+            while(counter > args[0]):
+                if args[1] > 0:
+                    return False
+                result.append(counter)
+                counter += args[1]
+            return result
+        else:
+            return False
+    else:
         return False
 
 
-def word_count(word):
-    return len(word.split())
+print(range_function(5))
+print(range_function(-5))
+print(range_function(0))
 
+print(range_function(10,7))
+print(range_function(11,22))
 
-def range_function(a, *args):
-    result = []
-    if len(args) == False:
-        print(range(0,a))
-        return range(0,a)
-    elif len(args) == 1:
-        print(range(a, args[0]))
-        return range(a,args[0])
-    elif len(args) == 2:
-        print(range(a, args[0], args[1]))
-        return range(a, args[0], args[1])
-
-
-
-range_function(5)
-range_function(0,7)
-range_function(0,10,2)
-
+print(range_function(0,10,2))
+print(range_function(0,10,-2))
+print(range_function(0, -10, -2))
 
 '''
 test_string = 'what is my name my name is paul soh hello world'
 query_for_test_string = 'name'
 
-test_list = ['a', 'ab', 'ac', 'add']
-query_for_test_list = 'ac'
+test_list = [1,2,'abc', 0.1, 'efd']
+query_for_test_list = 'a'
 
 test_dict = {'a':'b', 'c':'d'}
 query_for_test_dict = 'a'
@@ -44,7 +97,7 @@ query_for_test_dict = 'a'
 
 print(search(test_string, query_for_test_string))
 print(search(test_list, query_for_test_list))
-print(search(test_dict, query_for_test_dict))
+#print(search(test_dict, query_for_test_dict))
 
 
 #print(dosearch(string, 'is'))

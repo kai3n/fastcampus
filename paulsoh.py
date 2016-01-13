6d6b305c02e2493ec8b0a43d0c7171ff89c0845f
yongdoree <yongdoree@gmail.com>
2016-01-13 16:02:48 +0900

    Merge branch 'master' of https://github.com/kai3n/fastcampus
    # Conflicts:
    #	std1.py

--- paulsoh.py
+++ paulsoh.py
@@ -1,19 +1,27 @@
-# def search(list, word):
-#     if type(input) == str:
-#         if word in list:
-#             return True
-#     elif type(list) == tuple or type(list) == list:
-#         buff = ""
-#         for i in list:
-#             buff += str(i)
-#         if word in buff:
-#             return True
-#     return False
-#
-#
-# def word_count(word):
-#     return len(word.split())
-#
+test_list = [1,2,'abc', 0.1, 'efd']
+query_for_test_list = 'a'
+
+
+
+def search(sentance, word):
+    if type(sentance) == str and type(word) == str:
+        if word in sentance:
+            return True
+    elif type(sentance) == tuple or type(sentance) == list:
+        for i in range(0,len(sentance)):
+            if type(sentance[i]) == int or type(sentance[i]) == float:
+                if sentance[i] == word:
+                    return True
+            elif word in sentance[i]:
+                return True
+    return False
+
+print(search(test_list, query_for_test_list))
+
+
+def word_count(word):
+    return len(word.split())
+
 
 def range_function(a, *args):
     a = int(a)
@@ -82,25 +90,20 @@ print(range_function(0,10,2))
 print(range_function(0,10,-2))
 print(range_function(0, -10, -2))
 
-'''
+
 test_string = 'what is my name my name is paul soh hello world'
 query_for_test_string = 'name'
 
-test_list = [1,2,'abc', 0.1, 'efd']
-query_for_test_list = 'a'
+
 
 test_dict = {'a':'b', 'c':'d'}
 query_for_test_dict = 'a'
 
 #print(word_count(test_string))
-
-
 print(search(test_string, query_for_test_string))
-print(search(test_list, query_for_test_list))
-#print(search(test_dict, query_for_test_dict))
+print(search(test_dict, query_for_test_dict))
 
 
 #print(dosearch(string, 'is'))
 #print(dosearch(string, 'iadfasdfas'))
 
-'''

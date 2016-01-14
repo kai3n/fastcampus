06956b8ad7dec75b8491c756f7b00d056e521450
goohooh <goo_oo@naver.com>
2016-01-14 22:37:50 +0900

    3번까지 입니다

--- SeongPil_Lee.py
+++ SeongPil_Lee.py
@@ -1,90 +1,124 @@
-class Calculator():
-    def __init__(self):
+
+#TODO:upper, lower 과제,#
+#제출합니다
+
+def new_upper(string):
+    if type(string) == str:
         pass
-    def add(self, x, y):
-        self.x = x
-        self.y = y
-        return int(self.x + self.y)
-    def subtract(self, x, y):
-        #self.x = x
-        #self.y = y
-        return float(x - y)
-    def multiply(self, x, y):
-        return int(x * y)
-    def divide(self, x, y):
-        value = x / y
-        differ = value - int(value)
-        fill = 1 - differ
-        if x >= y:
-            if differ >= 0.5:
-                return int(value + fill)
-            else:
-                return int(value - differ)
+    else:
+        return None
+    chars = ''
+    for char in string:
+        asci = ord(char)
+        if 96 < asci and asci < 123:
+            asci -= 32
+            chars += chr(asci)
+        elif 64 < asci and asci < 91 or asci == 32:
+            chars += chr(asci)
         else:
-            if differ <= 0.5:
-                return int(value - differ)
-            else:
-                return int(value + fill)
+            return 'arg must be only str'
+    return chars
 
-    def expCalc(self,expStr):
-        """숫자 표현식을 문자열로 받아서 표현식에 대한 결과를 정수형으로 변환하는 함수이다. 난이도:★★★☆☆'
-        ex) expCalc('1+3-5')는 -1을 반환한다.
-        ex) expCalc('1+3*5')는 20을 반환한다.
-        ex) expCalc('1+3+5-0')는 9을 반환한다.
-        ex) expCalc('4+3+5/3')는 4을 반환한다.
-        """
-        # numList = []
-        # intList = []
-        # calList = []
-        # for char in expStr:
+print(new_upper('PlAnRi abLUeu'))
+
+def new_lower(string):
+    chars = ''
+    for char in string:
+        asci = ord(char)
+        if 64 < asci and asci < 91:
+            asci += 32
+            chars += chr(asci)
+        elif 96 < asci and asci < 123 or asci == 32:
+            chars += chr(asci)
+        else:
+            return 'arg must be only str'
+    return chars
+
+print(new_lower('SuPeR PoWeR'))
+
+
+# class Calculator():
+#     def __init__(self):
+#         pass
+#     def add(self, x, y):
+#         self.x = x
+#         self.y = y
+#         return int(self.x + self.y)
+#     def subtract(self, x, y):
+#         #self.x = x
+#         #self.y = y
+#         return float(x - y)
+#     def multiply(self, x, y):
+#         return int(x * y)
+#     def divide(self, x, y):
+#         value = x / y
+#
+#         if x > 0 and y > 0:
+
+
+
+
+        # value = x / y
+        # differ = value - int(value)
+        # fill = 1 - differ
         #
-        # return
-    def expCalcAdvanced(self,expStr):
-        """숫자 표현식을 문자열로 받아서(이 때 *와 / 연산자는 우선순위로 계산함, ()괄호에 대한 우선순위도 매김)표현식에
-        대한 결과를 소수점 둘째자리에서 반올림하여 실수형으로 변환하는 함수이다. 난이도:★★★★★
-        여기까지 실습시간 내에 완성하시는분은 제가 소고기 사드립니다.
+        # if x >= y:
+        #     if differ >= 0.5:
+        #         return int(value + fill)
+        #     else:
+        #         return int(value - differ)
+        # else:
+        #     if differ <= 0.5:
+        #         return int(value - differ)
+        #     else:
+        #         return int(value + fill)
 
-        ex) expCalc('1+3-5')는 -1을 반환한다.
-        ex) expCalc('1+3*5')는 16을 반환한다.
-        ex) expCalc('100+3+5-0')는 108을 반환한다.
-        ex) expCalc('(100+3)*5+300-(200+10)/2')는 710을 반환한다.
-        ex) expCalc('4+3+5/3')는 8.67을 반환한다.
-        """
-        return
+#     def expCalc(self,expStr):
+#         """숫자 표현식을 문자열로 받아서 표현식에 대한 결과를 정수형으로 변환하는 함수이다. 난이도:★★★☆☆'
+#         ex) expCalc('1+3-5')는 -1을 반환한다.
+#         ex) expCalc('1+3*5')는 20을 반환한다.
+#         ex) expCalc('1+3+5-0')는 9을 반환한다.
+#         ex) expCalc('4+3+5/3')는 4을 반환한다.
+#         """
+#         # numList = []
+#         # intList = []
+#         # calList = []
+#         # for char in expStr:
+#         #
+#         # return
+#     def expCalcAdvanced(self,expStr):
+#         """숫자 표현식을 문자열로 받아서(이 때 *와 / 연산자는 우선순위로 계산함, ()괄호에 대한 우선순위도 매김)표현식에
+#         대한 결과를 소수점 둘째자리에서 반올림하여 실수형으로 변환하는 함수이다. 난이도:★★★★★
+#         여기까지 실습시간 내에 완성하시는분은 제가 소고기 사드립니다.
+#
+#         ex) expCalc('1+3-5')는 -1을 반환한다.
+#         ex) expCalc('1+3*5')는 16을 반환한다.
+#         ex) expCalc('100+3+5-0')는 108을 반환한다.
+#         ex) expCalc('(100+3)*5+300-(200+10)/2')는 710을 반환한다.
+#         ex) expCalc('4+3+5/3')는 8.67을 반환한다.
+#         """
+#         return
+#
+# calc = Calculator()
+# # calc.expCalc('1+2+3')
+# print(calc.add(-4,3))
+# print(calc.subtract(-5.5,-3.1))
+# print(calc.divide(-2,3))
 
-calc = Calculator()
-# calc.expCalc('1+2+3')
-print(calc.add(4.5,3.3))
-print(calc.subtract(5.5,3.1))
-print(calc.divide(1,3))
-
-
-
-
-
-#######################################################################Before 01/14
+#
+# #######################################################################Before 01/14
 # import time
 #
 #
 # def elapsed_time(functor):
-# 	def decorated(*args,**kwargs):
-# 		start = time.time()
-# 		end = time.time()
-# 		print("elapsed time:%f" %(end - start))
-# 		return functor(*args,**kwargs)
-#
-#
-# 	return decorated
-#
-#
-# @elapsed_time
-# def wd_count(wd):
-#    wd_cnt = 0
-#    wds = str(wd)
-#    wd_cnt = len(wds.split(' '))
-#    return wd_cnt
-#
-# print(wd_count('asdf sdf'))
+#     def decorated(*args,**kwargs):
+#         start = time.time()
+#         functor(*args, **kwargs)
+#         end = time.time()
+#         print(end - start)
+#         result = functor(*args, **kwargs)
+#         return result
+#     return decorated
 #
 #
 # @elapsed_time
@@ -94,9 +128,8 @@ print(calc.divide(1,3))
 #     word_cnt = len(words.split(' '))
 #     return word_cnt
 #
-# print(word_count('sadf'))
+# print(word_count(' sd s sd sd sd '))
 #
-# print(word_count('ssd sd s sd sd sd '))
 # @elapsed_time
 # def search(string, word):
 #     if type(string) == str:
@@ -150,7 +183,7 @@ print(calc.divide(1,3))
 #     else:
 #         return 'Type Error'
 #
-# print(search(['apple', 1, 0.1, 8, 0.4,'power'], 0.4))
+# print(search(['apple', 1, 0.1, 8,4,123,566,885,3289,4654684,645654,21,3,21,8,65, 0.4,'power'], 0.3))
 #
 # def num_range(start, end, step):
 #     num_list = []
@@ -176,38 +209,6 @@ print(calc.divide(1,3))
 #         return 'Unknown Error'
 #
 # print(num_range(22, 22, 1))
-#
-#
-# def new_upper(string):
-#     if type(string) == str:
-#         pass
-#     else:
-#         return None
-#     chars = ''
-#     for char in string:
-#         asci = ord(char)
-#         if 96 < asci and asci < 123:
-#             asci -= 32
-#             chars += chr(asci)
-#         elif 64 < asci and asci < 91 or asci == 32:
-#             chars += chr(asci)
-#         else:
-#             return 'arg must be only str'
-#     return chars
-#
-# print(new_upper('PlAnRi abLUeu'))
-#
-# def new_lower(string):
-#     chars = ''
-#     for char in string:
-#         asci = ord(char)
-#         if 64 < asci and asci < 91:
-#             asci += 32
-#             chars += chr(asci)
-#         elif 96 < asci and asci < 123 or asci == 32:
-#             chars += chr(asci)
-#         else:
-#             return 'arg must be only str'
-#     return chars
-#
-# print(new_lower('SuPeR PoWeR'))
+
+
+

7106f0ef5808e5e2b16bf41e83d737310ea90c02
Mingun Pak <kaienmg@gmail.com>
2016-01-14 16:17:23 +0900

    1/13일 과제입니다.

--- kaien.py
+++ kaien.py
@@ -1,24 +1,42 @@
-# 다음 함수를 구현하시오
-def wd_upper(word):
-    """
-    이 함수는 word 문자열(str type) 전체를 대문자로 변환하는 함수이다.
-    매개변수가 str 타입이 아닐 시 None을 반환한다.
-    ex1) wd_upper('apple')를 호출할 시 'APPLE'를 반환한다.
-    ex2) wd_upper('Plane')를 호출할 시 'PLANE'를 반환한다.
-    ex3) wd_upper('AbCdE')를 호출할 시 'ABCDE'를 반환한다.
-    :param word:
-    :return word:
-    """
-    pass
-def wd_lower(word):
-    """
-    이 함수는 word 문자열(str type) 전체를 소문자로 변환하는 함수이다.
-    매개변수가 str 타입이 아닐 시 None을 반환한다.
-    ex1) wd_upper('apple')를 호출할 시 'apple'를 반환한다.
-    ex2) wd_upper('Plane')를 호출할 시 'plane'를 반환한다.
-    ex3) wd_upper('AbCdE')를 호출할 시 'abcde'를 반환한다.
+# class Calculator():
+#     def __init__(self):
+#         pass
+#     def add(self, x, y):
+#         '두 매개변수 x, y를 더해서 결과를 정수형으로 반환하는 함수이다. 난이도:★☆☆☆☆'
+#         return
+#     def subtract(self, x, y):
+#         '두 매개변수 x, y를 빼서 결과를 실수형으로 반환하는 함수이다. 난이도:★☆☆☆☆'
+#         return
+#     def multiply(self, x, y):
+#         '두 매개변수 x, y를 곱해서 결과를 정수형으로 반환하는 함수이다. 난이도:★☆☆☆☆'
+#         return
+#     def divide(self, x, y):
+#         '두 매개변수 x, y를 나눠서 결과를 소수점 첫째자리에서 반올림하여 정수형으로 반환하는 함수이다. 난이도:★★☆☆☆'
+#         return
+#     def expCalc(self,expStr):
+#         """숫자 표현식을 문자열로 받아서 표현식에 대한 결과를 정수형으로 변환하는 함수이다. 난이도:★★★☆☆'
+#         ex) expCalc('1+3-5')는 -1을 반환한다.
+#         ex) expCalc('1+3*5')는 20을 반환한다.
+#         ex) expCalc('1+3+5-0')는 9을 반환한다.
+#         ex) expCalc('4+3+5/3')는 4을 반환한다.
+#         """
+#         return
+#     def expCalcAdvanced(self,expStr):
+#         """숫자 표현식을 문자열로 받아서(이 때 *와 / 연산자는 우선순위로 계산함, ()괄호에 대한 우선순위도 매김)표현식에
+#         대한 결과를 소수점 둘째자리에서 반올림하여 실수형으로 변환하는 함수이다. 난이도:★★★★★
+#         여기까지 실습시간 내에 완성하시는분은 제가 소고기 사드립니다.
+#
+#         ex) expCalc('1+3-5')는 -1을 반환한다.
+#         ex) expCalc('1+3*5')는 16을 반환한다.
+#         ex) expCalc('100+3+5-0')는 108을 반환한다.
+#         ex) expCalc('(100+3)*5+300-(200+10)/2')는 710을 반환한다.
+#         ex) expCalc('4+3+5/3')는 8.67을 반환한다.
+#         """
+#         return
+#
+# > > > calc = Calculator()
+# > > > calc.add(1,2)
+# > > > calc.subtract(3,2)
 
-    :param word:
-    :return word:
-    """
-    pass
+
+

b3c7c345d8288ed9058373e7f1b7294acefcd702
JEON <MJ@JEONui-MacBook-Pro.local>
2016-01-14 17:35:37 +0900

    클래스 구현

--- mhjeon.py
+++ mhjeon.py
@@ -196,14 +196,21 @@ class Calculator():
         return int(x * y)
     def divide(self, x, y):
         '두 매개변수 x, y를 나눠서 결과를 소수점 첫째자리에서 반올림하여 정수형으로 반환하는 함수이다. 난이도:★★☆☆☆'
+
         split_num = str(x/y).split(".")
         num = int(split_num[0])
         digit_to_compare = int(split_num[1][0])
 
-        if digit_to_compare >= 5:
-            return num +1
+        if num > 0:
+            if digit_to_compare >= 5:
+                return num + 1
 
-        return num
+            return num
+        else:
+            if digit_to_compare >= 5:
+                return num - 1
+
+            return num
 
     def expCalc(self,expStr):
         """숫자 표현식을 문자열로 받아서 표현식에 대한 결과를 정수형으로 변환하는 함수이다. 난이도:★★★☆☆'
@@ -212,22 +219,24 @@ class Calculator():
         ex) expCalc('1+3+5-0')는 9을 반환한다.
         ex) expCalc('4+3+5/3')는 4을 반환한다.
         """
-
-
-        ints=[]
-        cal_ints = []_
+        '''
+        ints = []
         ops=[]
-        digit=0
-        sum = 0
+        cal_result = ""
+        empty_string = ""
 
         for element in expStr:
-            if type(element) == int:
-                ints += element
-            else:
+            try:
+                elem = int(element)
+                empty_string += str(elem)
+            except SyntaxError:
+                ints.append(empty_string)
+                empty_string = ""
+                ops += elem
 
-
-        return
-
+        cal_result=""
+        for range(len(ints))
+        '''
 
     def expCalcAdvanced(self,expStr):
         """숫자 표현식을 문자열로 받아서(이 때 *와 / 연산자는 우선순위로 계산함, ()괄호에 대한 우선순위도 매김)표현식에
@@ -246,4 +255,4 @@ calc = Calculator()
 print(calc.add(1,2))
 calc.subtract(3,2)
 print(calc.multiply(4,3))
-print(calc.divide(18,5))
+print(calc.divide(-16,5))

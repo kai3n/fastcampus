677273ef90e7a1242668f74d6246f99cee1d6a7f
sodarain730 <sodarain730@gmail.com>
2016-01-25 17:53:43 +0900

    array_stack

--- leejeonghyeon/array_stack.py
+++ leejeonghyeon/array_stack.py
@@ -4,24 +4,31 @@ class Empty(Exception):
 class ArrayStack:
 
     def __init__(self):
+        print("start")
         self._data = []
+        print("end")
 
     def __len__(self):
-        pass
+        return len(self._data)
 
     def is_empty(self):
-        if len(self._data) ==0:
+        if len(self._data) <= 0:
             return True
+        else:
+            return False
 
     def push(self, e):
-        stack_len =len(self._data)
-        self._data.insert((stack_len+1),e)
+        stack_len = len(self._data)
+        self._data.insert((stack_len+1), e)
         return self._data
 
     def top(self):
         return self._data[len(self._data)-1]
 
     def pop(self):
-        pop_e = self._data[len(self._data)-1]
-        del self._data[len(self._data)-1]
+        if len(self._data) <= 0:
+            raise Empty("error")
+        else:
+            pop_e = self._data[len(self._data)-1]
+            del self._data[len(self._data)-1]
         return pop_e

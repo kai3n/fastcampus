973b8ab88b3bf80faafb385e071c1285914b52af
seungkwon <yevgnenll@gmail.com>
2016-01-25 22:54:41 +0900

    복습

---
+++ seungkwon/array_queue.py
@@ -0,0 +1,53 @@
+class Empty(Exception):
+    pass
+
+
+class array_queue():
+    def __init__(self):
+        self.size = 0
+        self.data = list()
+
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, e):
+        self.data.append(e)
+        self.size += 1
+
+    def dequeue(self):
+        res = array_queue()
+        try:
+            res = self.data[0]
+            del self.data[0]
+        except:
+            raise Empty
+        self.size -= 1
+        return res
+
+    def first(self):
+        if self.size == 0:
+            raise Empty
+        else:
+            return self.data[0]
+
+    def is_empty(self):
+        if self.size == 0:
+            return False
+        else:
+            return True
+
+mQueue = array_queue()
+print(mQueue.is_empty())
+mQueue.enqueue(1)
+mQueue.enqueue(2)
+mQueue.enqueue(3)
+mQueue.enqueue(4)
+mQueue.enqueue(5)
+print(mQueue.data)
+mQueue.dequeue()
+mQueue.dequeue()
+print(mQueue.data)
+print(mQueue.is_empty())
+print(len(mQueue))
+mQueue.dequeue()
+print(mQueue.first())
--- seungkwon/array_stack.py
+++ seungkwon/array_stack.py
@@ -1,6 +1,57 @@
 class Empty(Exception):
     pass
 
+class makeStack():
+
+    def __init__(self):
+        self.size =0
+        self.newstack =list()
+
+    def __len__(self):
+        return len(self.newstack)
+
+    def push(self, data):
+        try:
+            self.newstack.append(data)
+        except:
+            raise Empty
+        self.size += 1
+
+    def pop(self):
+        try:
+            mid = self.newstack[self.size-1]
+            del self.newstack[self.size-1]
+            self.size -= 1
+            return mid
+        except:
+            raise Exception
+
+    def is_empty(self):
+        if self.size == 0:
+            return False
+        else:
+            return True
+
+    def top(self):
+
+        if self.size == 0:
+            raise Empty
+
+        return self.newstack[self.size-1]
+
+mStack = makeStack()
+mStack.push(5)
+mStack.push(3)
+mStack.push(3)
+mStack.push(3)
+mStack.pop()
+print("ddd----------r",len(mStack))
+print("len",mStack.__len__)
+print(mStack.is_empty())
+print (mStack)
+
+
+
 class stack_custom():
 
 
@@ -9,33 +60,33 @@ class stack_custom():
 
     def __init__(self):
         self._data = list()
-        self.__len__ = 0
+        self.size = 0
 
     def push(self, data):
         try:
             self._data.append(data)
         except:
             raise Exception
-        self.__len__ += 1
+        self.size += 1
 
     def is_empty(self):
-        if self.__len__ == 0:
+        if self.size == 0:
             return False
         else:
             return True
 
 
     def pop(self):
-        mid = self._data[self.__len__-1]
+        mid = self._data[self.size-1]
         try:
-            del self._data[self.__len__-1]
+            del self._data[self.size-1]
         except:
             raise Exception
-        self.__len__ -= 1
+        self.size -= 1
         return mid
 
     def top(self):
-        return self._data[self.__len__-1]
+        return self._data[self.size-1]
 
 # mStack = stack_custom()
 # mStack.push(5)
@@ -53,7 +104,6 @@ fir = "()(()){([])}"
 sec = "((()(()){([)])}))"
 third = ")(()){([])}"
 
-mStack = stack_custom()
 for a in third:
     if a == "(" or a == "{" or a == "[":
         mStack.push(a)
@@ -65,8 +115,9 @@ for a in third:
         elif mStack.top() == "[" and a == "]":
             mStack.pop()
     else:
+        # 처음 ), }, ] 가 왔을때 바로 입력부터 해준다
         mStack.push(a)
-    print(mStack._data)
+    print(mStack.newstack)
 
 if len(mStack) == 0:
     print(True)

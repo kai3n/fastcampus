01ac742a0d3d9a142264e66fa43a42da43d3c39b
autumnhan <genius2614@gmail.com>
2016-01-25 17:39:35 +0900

    array_stack!

--- Ahreumhan/array_stack.py
+++ Ahreumhan/array_stack.py
@@ -22,9 +22,13 @@ class ArrayStack:
         self._data.append(e)
 
     def top(self):
-        self._data[len(self._data)-1]
+        return self._data[len(self._data)-1]
 
     def pop(self):
-        self._data[len(self._data)-1]
-        del self._data[len(self._data)-1]
+
+        if len(self._data) == 0:
+            raise Exception
+        else:
+            self._data[len(self._data)-1]
+            del self._data[len(self._data)-1]
         return self._data[len(self._data)-1]

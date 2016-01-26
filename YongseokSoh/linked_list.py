22ce53e923d7fb6bb18eb48b5f109cfaf1306bf1
yongdoree <yongdoree@gmail.com>
2016-01-26 15:14:25 +0900

    LinkedList Implementation ver 0.2 1. Added linked_list.py

--- YongseokSoh/linked_list.py
+++ YongseokSoh/linked_list.py
@@ -14,22 +14,37 @@ class LinkedList:
     def __len__(self):
         return self._size
 
-    def __str__(self):
-        return (str(self._head), str(self._tail), str(self._size))
+    def print_list(self):
+        probe = self._head
+        while probe is not None:
+            print(probe._element)
+            probe = probe.next
 
     def is_empty(self):
-        return bool(self._size)
+        return not bool(self._size)
 
-    def add_first(self, node):
-        node.next = self._head
-        self._head = node
-        self._size += 1
+    def add_first(self, element):
+        node = LinkedList.Node(element, None)
+        if self._size is 0:
+            self._head = node
+            self._tail = node
+            self._size = 1
+        else:
+            node.next = self._head
+            self._head = node
+            self._size += 1
 
-    def add_last(self, node):
-        node.next = None
-        self._tail.next = node
-        self._tail = node
-        self._size += 1
+    def add_last(self, element):
+        node = LinkedList.Node(element, None)
+        if self._size is 0:
+            self._head = node
+            self._tail = node
+            self._size = 1
+        else:
+            node.next = None
+            self._tail.next = node
+            self._tail = node
+            self._size += 1
 
     def remove_first(self):
         if self._head is None:
@@ -43,20 +58,33 @@ class LinkedList:
             print("List is empty")
         else:
             probe = self._head
-            while probe._next is not self._tail:
-                probe = probe._next
-            probe._next = None
+            while probe.next is not self._tail:
+                probe = probe.next
+            probe.next = None
             self._tail = probe
             self._size -= 1
 
     def head(self):
-        return self._head
+        return self._head._element
 
     def tail(self):
-        return self._tail
+        return self._tail._element
 
 
-l = LinkedList()
+lst = LinkedList()
+#print(lst.is_empty()) #True
+lst.add_first(1)  #1
+#print(lst.head()) #return 1
+lst.add_first(2)  #2->1
+lst.add_first(3)  #3->2->1
+#print(lst.tail()) #return 1
+lst.add_last(4)  #3->2->1->4
+lst.add_last(5)  #3->2->1->4->5
+lst.add_last(6)  #3->2->1->4->5->6
+lst.print_list()
+lst.remove_first()  #2->1->4->5->6
 
-l.Node()
-
+lst.remove_last()  #2->1->4->5
+lst.print_list()  #2->1->4->5
+#print(lst.is_empty()) #False
+len(lst)  #return 4

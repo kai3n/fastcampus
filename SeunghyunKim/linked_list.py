7d21a7ce4aaa977b02fdde8d49a4d324f68deab8
kelon <jojo600@gmail.com>
2016-01-26 16:13:01 +0900

    commit

--- SeunghyunKim/linked_list.py
+++ SeunghyunKim/linked_list.py
@@ -1,10 +1,10 @@
-class Linkedlist:
+class LinkedList:
 
     class _Node:
 
-        def __init__(self, element, next):
+        def __init__(self, element):
             self._element = element
-            self._next = next
+            self._next = None
 
     def __init__(self):
 
@@ -23,19 +23,20 @@ class Linkedlist:
 
     def add_first(self, e):
 
-        x = self._Node(e, None)
+        x = self._Node(e)
 
-        if (self._size == 0):
+        if (self._head == None):
             self._head = e
             self._tail = e
         else:
             x._next = self._head
             self._head = x
-            self._size += 1
+
+        self._size += 1
 
     def add_last(self, e):
 
-        x = self._Node(e, None)
+        x = self._Node(e)
 
         if (self._size == 0) :
             self.add_first(e)
@@ -46,8 +47,12 @@ class Linkedlist:
 
 
     def remove_first(self):
-        self._head = self._head._next
-        self._size -= 1
+
+        if (self._size == 0):
+            print("error")
+        else:
+            self._head = self._head._next
+            self._size -= 1
 
     def remove_last(self):
         pass
@@ -61,8 +66,8 @@ class Linkedlist:
     def print_list(self):
         pass
 
-lst = Linkedlist()
-print(lst.is_empty()) #Ture
+lst = LinkedList()
+print(lst.is_empty()) #True
 lst.add_first(1)
 print(lst.head())   #1
 lst.add_first(2)
@@ -72,3 +77,10 @@ print(lst.head())   #3
 print(lst.tail())   #1
 lst.add_last(4)
 print(lst.tail())   #4
+lst.add_last(5)     #3->2->1->4->5
+lst.add_last(6)     #3->2->1->4->5->6
+lst.remove_first()  #2->1->4->5->6
+lst.remove_last()   #2->1->4->5
+lst.print_list()    #2->1->4->5
+print(lst.is_empty()) #False
+len(lst)            #return 4

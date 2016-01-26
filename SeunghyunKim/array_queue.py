40d61cce29a7082ca562b4a62f5b1ad47f7cfa02
kelon <jojo600@gmail.com>
2016-01-26 10:45:24 +0900

    commit

--- SeunghyunKim/array_queue.py
+++ SeunghyunKim/array_queue.py
@@ -1,18 +1,41 @@
 class Empty:
-    def error(x):
-        if(len(x)==0):
+    def error(arr):
+        if(len(arr)==0):
             print("error")
 
-class ArrayQueues:
+class ArrayQueue:
+
+    DEFAULT_CAPACITY = 10
+
     def __init__(self):
-        pass
-    def len(self):
+
+        self._data = [None] * ArrayQueue.DEFAULT_CAPACITY
+        self.size = 0
+        self._front = 0
+
+    def __len__(self):
         return len(self)
+
+    def is_empty(self):
+        if self!=0:
+            return False
+        else:
+            return True
+
+    def first(self):
+        if len(self) == 0:
+            raise Empty.error(self)
+        else:
+            return self[0]
+
+    def dequeue(self):
+        if len(self) == 0:
+            raise Empty.error(self)
+        else:
+            n = self[0]
+            del self[0]
+            return n
+
     def enqueue(self, x):
-        pass
-    def dequeue(self, x):
-        pass
-    def first(self, x):
-        pass
-    def is_empty(self, x):
-        pass
+        self.append(x)
+
